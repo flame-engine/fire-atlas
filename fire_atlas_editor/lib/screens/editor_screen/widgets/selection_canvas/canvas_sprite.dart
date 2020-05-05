310b81a7443562b599bb1f3bb3a874b6226655ce
Erick Zanardo <erickzanardoo@gmail.com>
2020-05-05 19:41:27 -0300

    Improving canvas background

--- fire_atlas_editor/lib/screens/editor_screen/widgets/selection_canvas/canvas_sprite.dart
+++ fire_atlas_editor/lib/screens/editor_screen/widgets/selection_canvas/canvas_sprite.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flame/sprite.dart';
+import 'package:tinycolor/tinycolor.dart';
 
 class CanvasSprite extends StatelessWidget {
   final Sprite sprite;
@@ -33,6 +34,7 @@ class CanvasSprite extends StatelessWidget {
             selectionEnd,
 
             Theme.of(ctx).primaryColor,
+            Theme.of(ctx).dividerColor,
         )),
     );
   }
@@ -48,6 +50,7 @@ class _CanvasSpritePainer extends CustomPainter {
   final Offset _selectionEnd;
 
   Color _selectionColor;
+  Color _gridTileColor;
 
   _CanvasSpritePainer(
       this._sprite,
@@ -60,6 +63,7 @@ class _CanvasSpritePainer extends CustomPainter {
       this._selectionStart,
       this._selectionEnd,
       this._selectionColor,
+      this._gridTileColor,
   );
 
   @override
@@ -73,20 +77,61 @@ class _CanvasSpritePainer extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
+    // Background
+    canvas.drawRect(
+        Rect.fromLTWH(0, 0, size.width, size.height),
+        Paint()
+          ..color = TinyColor(
+              _gridTileColor.withOpacity(1)
+          ).lighten(60).color
+    );
+
     canvas.save();
     canvas.translate(_x, _y);
     canvas.scale(_scale, _scale);
 
-    _sprite.renderRect(
-        canvas,
-        Rect.fromLTWH(
-            0,
-            0,
-            _sprite.size.x,
-            _sprite.size.y,
-        )
+    final spriteRect = Rect.fromLTWH(
+        0,
+        0,
+        _sprite.size.x,
+        _sprite.size.y,
     );
 
+    // Background outline
+    canvas.drawRect(
+        spriteRect.inflate(1.0),
+        Paint()
+          ..color = TinyColor(
+              _gridTileColor.withOpacity(1)
+          ).lighten(20).color
+    );
+
+    // Checker board
+    final rowCount = _sprite.size.y / _tileSize;
+    final columnCount = _sprite.size.x / _tileSize;
+
+    final darkTilePaint = Paint()
+        ..color = TinyColor(_gridTileColor.withOpacity(1)).lighten(70).color;
+    final lightTilePaint = Paint()
+        ..color = TinyColor(_gridTileColor.withOpacity(1)).lighten(90).color;
+
+    for (var y = 0.0; y < rowCount; y++) {
+      final m = y % 2;
+      final p1 =  m == 0 ? darkTilePaint : lightTilePaint;
+      final p2 =  m == 0 ? lightTilePaint : darkTilePaint;
+
+      for (var x = 0.0; x < columnCount; x++) {
+        canvas.drawRect(
+            Rect.fromLTWH(x * _tileSize, y * _tileSize, _tileSize.toDouble(), _tileSize.toDouble()),
+            x % 2 == 0 ? p1 : p2 
+        );
+      }
+    }
+
+    // Sprite itself
+    _sprite.renderRect(canvas, spriteRect);
+
+    // Selection
     if (_selectionStart != null && _selectionEnd != null) {
       final size = _selectionEnd - _selectionStart + Offset(1, 1);
       canvas.drawRect(
