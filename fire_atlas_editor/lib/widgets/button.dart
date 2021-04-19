5c9434651fc3472ff764ee475ce52218192adb4b
Erick Zanardo <erickzanardoo@gmail.com>
2021-04-19 11:58:56 -0300

    fixing concat image

--- fire_atlas_editor/lib/widgets/button.dart
+++ fire_atlas_editor/lib/widgets/button.dart
@@ -4,11 +4,15 @@ class FButton extends StatelessWidget {
   final bool selected;
   final String label;
   final bool disabled;
+  final double? width;
+  final EdgeInsets? padding;
   final void Function() onSelect;
 
   FButton({
     this.selected = false,
     this.disabled = false,
+    this.width,
+    this.padding,
     required this.label,
     required this.onSelect,
   });
@@ -19,14 +23,24 @@ class FButton extends StatelessWidget {
 
     final color = selected ? theme.primaryColor : theme.buttonColor;
 
-    return Opacity(
+    return Container(
+      padding: padding,
+      child: Opacity(
         opacity: disabled ? 0.6 : 1,
         child: ElevatedButton(
           style: ButtonStyle(backgroundColor: MaterialStateProperty.all(color)),
           onPressed: () {
             if (!disabled) onSelect();
           },
-          child: Text(label),
-        ));
+          child: Container(
+            width: width,
+            child: Text(
+              label,
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
