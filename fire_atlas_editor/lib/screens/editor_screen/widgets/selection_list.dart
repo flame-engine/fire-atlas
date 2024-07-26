da7416814fb3739049ec46d086052ff1b14308b1
Erick Zanardo <erickzanardoo@gmail.com>
2024-07-26 16:22:06 -0300

    feat: adding grouping to selections

--- fire_atlas_editor/lib/screens/editor_screen/widgets/selection_list.dart
+++ fire_atlas_editor/lib/screens/editor_screen/widgets/selection_list.dart
@@ -1,5 +1,6 @@
 import 'package:equatable/equatable.dart';
 import 'package:fire_atlas_editor/screens/editor_screen/widgets/delete_selection_modal.dart';
+import 'package:fire_atlas_editor/screens/editor_screen/widgets/edit_selection_folder_modal.dart';
 import 'package:fire_atlas_editor/screens/editor_screen/widgets/selection_canvas/selection_form.dart';
 import 'package:fire_atlas_editor/store/actions/atlas_actions.dart';
 import 'package:fire_atlas_editor/store/actions/editor_actions.dart';
@@ -15,22 +16,40 @@ class _SelectionListSlice extends Equatable {
   final FireAtlas? currentAtlas;
   final BaseSelection? selectedSelection;
 
-  const _SelectionListSlice(this.currentAtlas, this.selectedSelection);
+  final List<String?> _groups;
+
+  _SelectionListSlice(this.currentAtlas, this.selectedSelection)
+      : _groups =
+            currentAtlas?.selections.values.map((e) => e.group).toList() ??
+                const [];
 
   @override
-  List<Object?> get props => [currentAtlas?.id, selectedSelection?.id];
+  List<Object?> get props => [
+        currentAtlas?.id,
+        selectedSelection?.id,
+        _groups,
+      ];
 }
 
-class SelectionList extends StatelessWidget {
+class SelectionList extends StatefulWidget {
   const SelectionList({super.key});
+
+  @override
+  State<SelectionList> createState() => _SelectionListState();
+}
+
+class _SelectionListState extends State<SelectionList> {
+  final _openGroups = <String>[];
 
   @override
   Widget build(_) {
     return SliceWatcher<FireAtlasState, _SelectionListSlice>(
-      slicer: (state) => _SelectionListSlice(
-        state.currentAtlas,
-        state.selectedSelection,
-      ),
+      slicer: (state) {
+        return _SelectionListSlice(
+          state.currentAtlas,
+          state.selectedSelection,
+        );
+      },
       builder: (ctx, store, slice) {
         final currentAtlas = slice.currentAtlas;
 
@@ -41,95 +60,198 @@ class SelectionList extends StatelessWidget {
         void select(BaseSelection selection) =>
             store.dispatch(SelectSelectionAction(selection: selection));
 
+        final groupedSelections = currentAtlas.selections.values.fold(
+          <String, List<BaseSelection>>{},
+          (map, selection) {
+            final group = selection.group ?? '';
+            if (!map.containsKey(group)) {
+              map[group] = [];
+            }
+
+            map[group]!.add(selection);
+            return map;
+          },
+        );
+
+        final rootLevelGroup = groupedSelections[''] ?? <BaseSelection>[];
+
+        final folders = Map.fromEntries(
+          groupedSelections.entries
+              .where((entry) => entry.key.isNotEmpty)
+              .toList(),
+        );
+
         return FContainer(
           padding: const EdgeInsets.all(5),
           child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                const FSubtitleTitle(title: 'Selections'),
-                ...currentAtlas.selections.isEmpty
-                    ? [const FLabel(label: 'No selections yet')]
-                    : currentAtlas.selections.values
-                        .map((selection) {
-                          return Container(
-                            margin: const EdgeInsets.all(10),
-                            padding: const EdgeInsets.only(bottom: 5),
-                            decoration: BoxDecoration(
-                              border: Border(
-                                bottom: BorderSide(
-                                  width: 2,
-                                  color: Theme.of(ctx).dividerColor,
-                                ),
-                              ),
+            child: Builder(
+              builder: (context) {
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    const FSubtitleTitle(title: 'Selections'),
+                    if (currentAtlas.selections.isEmpty)
+                      const FLabel(label: 'No selections yet'),
+                    for (final selection in rootLevelGroup)
+                      _SelectionEntry(
+                        selection: selection,
+                        slice: slice,
+                        select: select,
+                        store: store,
+                      ),
+                    for (final folderEntry in folders.entries)
+                      Container(
+                        margin: const EdgeInsets.only(top: 10),
+                        padding: const EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          border: Border(
+                            bottom: BorderSide(
+                              width: 2,
+                              color: Theme.of(context).dividerColor,
                             ),
-                            child: GestureDetector(
-                              onTap: () => select(selection),
+                          ),
+                        ),
+                        child: Column(
+                          children: [
+                            GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  if (_openGroups.contains(folderEntry.key)) {
+                                    _openGroups.remove(folderEntry.key);
+                                  } else {
+                                    _openGroups.add(folderEntry.key);
+                                  }
+                                });
+                              },
                               child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
                                 children: [
-                                  Expanded(
-                                    child: Text(
-                                      selection.id,
-                                      style: TextStyle(
-                                        fontWeight:
-                                            (slice.selectedSelection?.id ==
-                                                    selection.id)
-                                                ? FontWeight.bold
-                                                : FontWeight.normal,
-                                      ),
-                                    ),
+                                  Icon(
+                                    _openGroups.contains(folderEntry.key)
+                                        ? Icons.folder_open
+                                        : Icons.folder,
                                   ),
-                                  Row(
-                                    children: [
-                                      if (selection is AnimationSelection)
-                                        FIconButton(
-                                          iconData: Icons.edit,
-                                          onPress: () {
-                                            select(selection);
-                                            store.dispatch(
-                                              OpenEditorModal(
-                                                SelectionForm(
-                                                  editingSelection: selection,
-                                                ),
-                                                400,
-                                                500,
-                                              ),
-                                            );
-                                          },
-                                          tooltip: 'Edit selection',
-                                        )
-                                      else
-                                        Container(),
-                                      FIconButton(
-                                        iconData: Icons.cancel,
-                                        onPress: () {
-                                          select(selection);
-                                          store.dispatch(
-                                            OpenEditorModal(
-                                              const DeleteSelectionModal(),
-                                              300,
-                                              200,
-                                            ),
-                                          );
-                                        },
-                                        tooltip: 'Edit selection',
-                                      ),
-                                    ],
-                                  ),
+                                  const SizedBox(width: 5),
+                                  Text(folderEntry.key),
                                 ],
                               ),
                             ),
-                          );
-                        })
-                        .toList()
-                        .cast<Widget>(),
-              ],
+                            if (_openGroups.contains(folderEntry.key))
+                              for (final selection in folderEntry.value)
+                                _SelectionEntry(
+                                  selection: selection,
+                                  slice: slice,
+                                  select: select,
+                                  store: store,
+                                ),
+                          ],
+                        ),
+                      ),
+                  ],
+                );
+              },
             ),
           ),
         );
       },
+    );
+  }
+}
+
+class _SelectionEntry extends StatelessWidget {
+  const _SelectionEntry({
+    required this.selection,
+    required this.slice,
+    required this.select,
+    required this.store,
+  });
+
+  final BaseSelection selection;
+  final _SelectionListSlice slice;
+  final void Function(BaseSelection) select;
+  final SlicesStore<FireAtlasState> store;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(10),
+      padding: const EdgeInsets.only(bottom: 5),
+      decoration: BoxDecoration(
+        border: Border(
+          bottom: BorderSide(
+            width: 2,
+            color: Theme.of(context).dividerColor,
+          ),
+        ),
+      ),
+      child: GestureDetector(
+        onTap: () => select(selection),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(
+              child: Text(
+                selection.id,
+                style: TextStyle(
+                  fontWeight: (slice.selectedSelection?.id == selection.id)
+                      ? FontWeight.bold
+                      : FontWeight.normal,
+                ),
+              ),
+            ),
+            Row(
+              children: [
+                if (selection is AnimationSelection)
+                  FIconButton(
+                    iconData: Icons.edit,
+                    onPress: () {
+                      select(selection);
+                      store.dispatch(
+                        OpenEditorModal(
+                          SelectionForm(
+                            editingSelection: selection as AnimationSelection,
+                          ),
+                          400,
+                          500,
+                        ),
+                      );
+                    },
+                    tooltip: 'Edit selection',
+                  ),
+                FIconButton(
+                  iconData: Icons.folder,
+                  onPress: () {
+                    store.dispatch(
+                      OpenEditorModal(
+                        EditSelectionFolderModal(
+                          selectionId: selection.id,
+                          folderId: selection.group,
+                        ),
+                        500,
+                        280,
+                      ),
+                    );
+                  },
+                  tooltip: 'Move into Folder',
+                ),
+                FIconButton(
+                  iconData: Icons.cancel,
+                  onPress: () {
+                    select(selection);
+                    store.dispatch(
+                      OpenEditorModal(
+                        const DeleteSelectionModal(),
+                        300,
+                        200,
+                      ),
+                    );
+                  },
+                  tooltip: 'Delete selection',
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
